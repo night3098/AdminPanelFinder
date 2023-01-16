71943609445800b9c114cd4ef9d9386da710b857
night3098 <122676310+night3098@users.noreply.github.com>
2023-01-16 10:39:19 +0000

    Update SearchAdminPanel.py

--- SearchAdminPanel.py
+++ SearchAdminPanel.py
@@ -11,19 +11,20 @@ admins = f.read().split('\n')
 f.close()
 
 elements = str(len(admins))
-
+print("Найдено: " + elements + " шаблонов")
 
 url = input("Введите домен сайта https://")
 #for admin in admins:
 #for admin in tqdm(admins):
 for admin in tqdm_gui(admins, desc="search admin panel"):
     adminurl = "https://"+url+"/"+admin
-    status = requests.get(adminurl, timeout = 5).status_code
+    status = requests.get(adminurl).status_code
     if status == 200:
         print("найдено что-то похожее на админку: ", adminurl)
         x = input("окрыть в браузере?  Y/N : ")
         if x.lower() == 'y':
             webbrowser.open(adminurl)
-        break
+        if x.lower() == 'n':
+            pass
     else:
         print(adminurl + " - error")
