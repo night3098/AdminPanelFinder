22f06340d849decd55e53959d0f9fade26f00c7c
night3098 <122676310+night3098@users.noreply.github.com>
2023-01-16 10:27:34 +0000

    Add files via upload

---
+++ SearchAdminPanel.py
@@ -0,0 +1,29 @@
+#  прога для поиска админок на сайтах
+
+import requests
+import webbrowser
+import pretty_errors
+
+from tqdm import tqdm, tqdm_gui
+
+f = open("admins_pages.txt", 'r', encoding = 'UTF-8')
+admins = f.read().split('\n')
+f.close()
+
+elements = str(len(admins))
+
+
+url = input("Введите домен сайта https://")
+#for admin in admins:
+#for admin in tqdm(admins):
+for admin in tqdm_gui(admins, desc="search admin panel"):
+    adminurl = "https://"+url+"/"+admin
+    status = requests.get(adminurl, timeout = 5).status_code
+    if status == 200:
+        print("найдено что-то похожее на админку: ", adminurl)
+        x = input("окрыть в браузере?  Y/N : ")
+        if x.lower() == 'y':
+            webbrowser.open(adminurl)
+        break
+    else:
+        print(adminurl + " - error")
